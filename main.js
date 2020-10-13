9ddc174354e9948f5b8a56c4ddd1fca88741372c
Manuel <manuel.devito@icloud.com>
2020-10-13 15:06:44 +0200

    Fine esercizio

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var nome_utente = prompt ('Come ti chiami?')
 var cognome_utente = prompt ("Qual'è il tuo cognome?")
 var colore_utente = prompt ("Qual'è il tuo colore preferito?")
-var anno_corrente = prompt ("Qual'è l'anno corrente?")
 
-document.getElementById('password').innerHTML = nome_utente + cognome_utente + colore_utente + anno_corrente;
+
+document.getElementById('password').innerHTML = nome_utente + cognome_utente + colore_utente + anno_corrente + '20';
